3a580741b8d845a9c8b25434d8ddd593b4745f93
singh3abhi <singh3abhi@gmail.com>
2025-01-12 23:03:02 +0530

    created card component and cart items

---
+++ src/app/pages/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, input } from '@angular/core';
+import { Product } from '../../../models/products.model';
+import { ButtonComponent } from '../../../components/button/button.component';
+import { CartService } from '../../../services/cart.service';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [ButtonComponent, ButtonComponent],
+  template: `
+    <div
+      class="bg-white shadow-md border rounded-xl p-6 flex gap-4 items-center"
+    >
+      <img [src]="item().image" class="w-[50px] h-[50px] object-contain" />
+      <div class="flex flex-col">
+        <span class="text-md font-bold">{{ item().title }}</span>
+        <span class="text-sm">{{ '$' + item().price }}</span>
+      </div>
+      <div class="flex-1"></div>
+      <app-button
+        label="Remove"
+        (buttonClicked)="cartService.removeFromCart(index())"
+      ></app-button>
+    </div>
+  `,
+  styles: ``,
+})
+export class CartItemComponent {
+  item = input.required<Product>();
+  index = input.required<number>();
+
+  cartService = inject(CartService);
+}
