9c622dcbedd2438bede064c13532d50a12d3e8cf
singh3abhi <singh3abhi@gmail.com>
2025-01-12 18:31:45 +0530

    Created Product List component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from "./pages/products-list/products-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ProductsListComponent],
   template: `
     <app-header/>
+    <app-products-list></app-products-list>
   `,
   styles: [],
 })
